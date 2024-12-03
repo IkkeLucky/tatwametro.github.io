1b50a1f97bf559f0579c46817ea2ca1fcf39a7c1
gon2410 <goonolivera@hotmail.com>
2024-12-03 15:21:17 -0300

    updated timer color

--- src/App.tsx
+++ src/App.tsx
@@ -4,53 +4,53 @@ import './App.css';
 
 const states = [
 	{
-	  name: 'Akash',
-	  background: 'bg-gradient-to-b from-gray-900 via-purple-900 to-violet-800', 
-	  descriptions: [
-		'Principio organico, elemento de destruccion y abolicion',
-		'Se deben evitar estos 24 minutos que vibra akash, ocurren grandes decepciones y esperanzas destruidas',
-		'Akash obra disolviendo, oponiendose y destruyendo todo l ocorporal. En el duerme el principio de todo ser, el enigma de toda destruccion y creacion',
-		'Retorno al estado primitivo, no debemos hacer nada, debemos aislarnos, quedarnos tranquilos en meditacion y pensamiento sobre el principio'
+		name: 'Akash',
+		background: 'bg-gradient-to-b from-gray-900 via-purple-900 to-violet-800', 
+		descriptions: [
+			'Principio organico, elemento de destruccion y abolicion',
+			'Se deben evitar estos 24 minutos que vibra akash, ocurren grandes decepciones y esperanzas destruidas',
+			'Akash obra disolviendo, oponiendose y destruyendo todo l ocorporal. En el duerme el principio de todo ser, el enigma de toda destruccion y creacion',
+			'Retorno al estado primitivo, no debemos hacer nada, debemos aislarnos, quedarnos tranquilos en meditacion y pensamiento sobre el principio'
+		]
+	},
+	{
+		name: 'Vayu',
+		background: 'bg-gradient-to-b from-blue-600 via-blue-400 to-cyan-300',
+		descriptions: [
+			'Primera manifestacion de la vida, representa el movimiento. Velocidad y movimiento, aire, vientos, navegacion y todo lo que sea avanzar',
+			'Ascension a montanas, ejercicio y todo relacionado a moverse debe elegirse en este Tatwa. Todo movimiento que se inicie en este Tatwa ayuda a no cansarse tanto',
+			'Aspectos relacionados con facultades intelectuales y espirituales. Mayor proteccion y desarrollo intelectual y mas sentido logico y juicios intuitivos',
+			'Vayu generalmente '
 	  ]
 	},
 	{
-	  name: 'Vayu',
-	  background: 'bg-gradient-to-b from-blue-600 via-blue-400 to-cyan-300',
-	  descriptions: [
-		'Primera manifestacion de la vida, representa el movimiento. Velocidad y movimiento, aire, vientos, navegacion y todo lo que sea avanzar',
-		'Ascension a montanas, ejercicio y todo relacionado a moverse debe elegirse en este Tatwa. Todo movimiento que se inicie en este Tatwa ayuda a no cansarse tanto',
-		'Aspectos relacionados con facultades intelectuales y espirituales. Mayor proteccion y desarrollo intelectual y mas sentido logico y juicios intuitivos',
-		'Vayu generalmente '
+		name: 'Tejas',
+		background: 'bg-gradient-to-b from-red-600 via-orange-500 to-yellow-400',
+		descriptions: [
+			'Aumento de energias, este es el momento mas activo y mas productivo del dia',
+			'Tatwa donde se encuentra la confianza absoluta en uno mismo',
+			'Momento donde el exceso de energias puede derivar en momentos agresivos, enojadizos, belicosos, celosos. Tejas es portador de luchas y discusiones',
+			'El principio de Tejas es caliente, cuando vibra este tatwas se eleva nuestra temperatura'
 	  ]
 	},
 	{
-	  name: 'Tejas',
-	  background: 'bg-gradient-to-b from-red-600 via-orange-500 to-yellow-400',
-	  descriptions: [
-		'Aumento de energias, este es el momento mas activo y mas productivo del dia',
-		'Tatwa donde se encuentra la confianza absoluta en uno mismo',
-		'Momento donde el exceso de energias puede derivar en momentos agresivos, enojadizos, belicosos, celosos. Tejas es portador de luchas y discusiones',
-		'El principio de Tejas es caliente, cuando vibra este tatwas se eleva nuestra temperatura'
+		name: 'Prithvi',
+		background: 'bg-gradient-to-b from-green-800 via-green-600 to-yellow-500', 
+		descriptions: [
+			'Brightness illuminates everything Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde optio repudiandae',
+			'Shadows retreat to the corners',
+			'A sense of clarity and vision prevails',
+			'The world is awash in radiance'
 	  ]
 	},
 	{
-	  name: 'Prithvi',
-	  background: 'bg-gradient-to-b from-green-800 via-green-600 to-yellow-500', 
-	  descriptions: [
-		'Brightness illuminates everything',
-		'Shadows retreat to the corners',
-		'A sense of clarity and vision prevails',
-		'The world is awash in radiance'
-	  ]
-	},
-	{
-	  name: 'Apas',
-	  background: 'bg-gradient-to-b from-yellow-200 via-yellow-100 to-white', 
-	  descriptions: [
-		'Tatwas donde se puede hacer abortar el mal, correspondiente al agua, donde las peleas no duran mucho tiempo',
-		'El tatwa Apas obra concentrando, buen momento de iniciar negocios, depositos de dinero o hacer especulaciones. Tatwa de la riqueza y opulencia',
-		'Tatwa del amor sexual y concepcion en el vientre de la madre. Tatwas que influencia al amor',
-		'Este momento trae paz, alegria, juego, gozo, baile y placeres de todas las clases'
+		name: 'Apas',
+		background: 'bg-gradient-to-b from-yellow-200 via-yellow-100 to-white', 
+		descriptions: [
+			'Tatwas donde se puede hacer abortar el mal, correspondiente al agua, donde las peleas no duran mucho tiempo',
+			'El tatwa Apas obra concentrando, buen momento de iniciar negocios, depositos de dinero o hacer especulaciones. Tatwa de la riqueza y opulencia',
+			'Tatwa del amor sexual y concepcion en el vientre de la madre. Tatwas que influencia al amor',
+			'Este momento trae paz, alegria, juego, gozo, baile y placeres de todas las clases'
 	  ]
 	}
 ]
