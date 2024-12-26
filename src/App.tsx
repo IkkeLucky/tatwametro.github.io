f8073b639be867336818dedad95c4c887fc7cff6
gon2410 <goonolivera@hotmail.com>
2024-12-26 16:05:39 -0300

    removed api usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-'use client'
 import { useState, useEffect, useRef } from 'react';
-import Calendar from './Calendar';
 import './App.css';
 import "react-datepicker/dist/react-datepicker.css";
+import { getSunrise } from "sunrise-sunset-js"
+import { CountryDropdown } from 'react-country-region-selector';
+import _data from "./assets/country-by-geo-coordinates.json";
+//import Calendar from './Calendar';
+
+interface Data {
+	country: string;
+	north: number;
+	south: number;
+	west: number;
+	east: number;
+}
+
+const data = _data as Data[];
 
 const states = [
 	{
@@ -58,8 +70,9 @@ const states = [
 ]
 
 
-function getState(sunriseTime: Date, actualTime: Date) {
+function getState(sunriseTime: Date) {
 	let state = 0;
+	const actualTime = new Date();
 	while (sunriseTime <= actualTime) {
 		//console.log(sunriseTime)
 		state += 1;
@@ -75,7 +88,8 @@ function getState(sunriseTime: Date, actualTime: Date) {
 devuelve la diferencia en segundos entre la hora de inicio del
 estado actual y la hora de inicio del siguiente
 */
-function getTime(sunriseTime: Date, actualTime: Date) {
+function getTime(sunriseTime: Date) {
+	const actualTime = new Date();
     while (sunriseTime < actualTime) {
         sunriseTime.setMinutes(sunriseTime.getMinutes() + 24);
     }
@@ -84,71 +98,38 @@ function getTime(sunriseTime: Date, actualTime: Date) {
     return Math.floor(diff / 1000);
 }
 
+
 function App() {
-	const [countdownTime, setCountdownTime] = useState(Number);
-	const [error, setError] = useState("");
+	const [country, setCountry] = useState(String);
 	const [currentState, setCurrentState] = useState<number | null>(null);
-	const [currentCountry, setCurrentCountry] = useState<string | null>(null);
+	const [onError, setOnError] = useState(String);
 	const countdownIntervalId = useRef(0);
-	const [stateChanged, setStateChanged] = useState(true);
+	const [stateNeedsToChange, setStateNeedsToChange] = useState(Boolean);
+	const [countdownTime, setCountdownTime] = useState(Number);
 	const [minutesRem, setMinutesRem] = useState(0);
 	const [secondsRem, setSecondsRem] = useState(0);
 
-	/*
-	Llama a la api SI O SI con las coordenadas de ubicacion correctas.
-	Hasta que no suceda esto se renderiza la pagina de "Cargando.."
-	De otra forma, devuelve un ubicacion y horario de amanecer cualquiera
-	
-	Lo malo es que esta el riesgo que en el cambio automatico de estado
-	pida la ubicacion de vuelta
-	*/
-
+	const [lat, setLat] = useState<number | null>(null);
+	const [lon, setLon] = useState<number | null>(null);
 	useEffect(() => {
-		if(navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition((position) => {
-				const lat = position.coords.latitude;
-				const lon = position.coords.longitude;
-				callAPI(lat, lon);
-				setStateChanged(true);
-			}, (error) => {
-				setError(error.message);
-			})
-		} else {
-			setError("Ubicacion no disponible");
-		}
-	}, [stateChanged])
-
-
-	function callAPI(lat: number, lon: number) {
-		const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=0e1414393ec8b28124eff5028e775326&units=metric`;
-		fetch(apiUrl)
-			.then((response) => {
-				console.log("called api")
-				if (response.ok) {
-					return response.json();
+		try {
+			if (country.length != 0) {
+				const choosedCountry = data.find((element) => element.country == country);
+				if (choosedCountry != null) {
+					const lat = (choosedCountry.north + choosedCountry.south) / 2
+					const lon = (choosedCountry.east + choosedCountry.west) / 2
+					const sunriseTime = getSunrise(lat, lon)
+					setCurrentState(getState(sunriseTime));
+					setCountdownTime(getTime(sunriseTime));
 				} else {
-					return Promise.reject(response);
+					setOnError("No pudimos encontrar información.")
 				}
-			})
-			.then((data) => {
-				//console.log(data)
-				const sunriseUnixTime = data.sys.sunrise;
-				//console.log(sunriseUnixTime)
-				const sunriseTime = new Date(sunriseUnixTime * 1000);
-				const actualTime = new Date();
+			}
+		} catch (error) {
+			setOnError("Error")
+		}
 
-				//console.log(sunriseTime);
-				setCurrentCountry(data.sys.country);
-				setCurrentState(getState(sunriseTime, actualTime));
-				setCountdownTime(getTime(sunriseTime, actualTime));
-
-				//console.log('called api');
-			})
-			.catch((error) => {
-				setError(error);
-			});
-	}
-
+	}, [country, stateNeedsToChange])
 
 	useEffect(() => {
 		return () => {
@@ -164,7 +145,11 @@ function App() {
 	useEffect(() => {
 		if (countdownTime < 0) {
 			doResetCountdown();
-			setStateChanged(false);
+			if (stateNeedsToChange == true) {
+				setStateNeedsToChange(false);
+			} else {
+				setStateNeedsToChange(true);
+			}
 		}
 	}, [countdownTime]);
 
@@ -205,48 +190,77 @@ function App() {
 
 	}, [countdownTime])
 
+	
+	function click() {
+		if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				setLat(position.coords.latitude)
+				setLon(position.coords.longitude)				
+			}, (error) => {
+				alert("Debe otorgarnos acceso a su ubicacion. Intente resetear los permisos del navegador");
+				console.log(error)
+			})
+		} else {
+			setOnError("Debe otorgarnos acceso a su ubicacion.");
+			console.log(onError)
+		}
+	}
+
+	useEffect(() => {
+		if (lat != null && lon != null) {
+			const sunriseTime = getSunrise(lat, lon)
+			setCurrentState(getState(sunriseTime));
+			setCountdownTime(getTime(sunriseTime));
+		}
+	}, [lat, stateNeedsToChange])
+
 	if(currentState == null) {
 		return (
+			<div className="w-full min-h-screen flex flex-col items-center justify-center p-4 sm:p-6 md:p-8">
+				<CountryDropdown value={country} onChange={(val) => setCountry(val)} />
+				<button className="btn border-green-500 p-1 mt-10" onClick={click}>Por ubicacion</button>
+			</div>
+		)
+	} else if (onError) {
+		return (
 			<div>
-				<p>Cargando...</p>
+				<p>Algo salió mal...</p>
 				<br />
 				<hr />
 				<br />
-				<p>No te olvides de activar la ubicacion para que la app funcione correctamente</p>
+				<p>Detalles: {onError}</p>
 			</div>
 		)
-	} else if (error) {
-		<div>
-			<p>Algo salió mal..</p>
-			<br />
-			<hr />
-			<br />
-			<p>Detalles: {error}</p>
-		</div>
+
 	} else {
-		const stateToShow = states[currentState - 1]; // Adjusting to 0-based index for our states array
+		try {
+			const stateToShow = states[currentState - 1]; // Adjusting to 0-based index for our states array
 		
-		return (
-			<div className={`w-full min-h-screen flex flex-col items-center justify-center p-4 sm:p-6 md:p-8 transition-colors duration-1000 ${stateToShow.background}`}> 		
-				
-				{countdownTime < 10 ? <h5 className="text-white">A punto de cambiar de estado...</h5> : ""}
-				{secondsRem < 10 ? <h5 className="text-white">Tiempo restante para el siguiente estado: {minutesRem}:0{secondsRem} </h5> : <h5 className="text-white">Tiempo restante para el siguiente estado: {minutesRem}:{secondsRem} </h5>}
-				<h5 className="text-3xl sm:text-4xl mb-6 sm:mb-8 text-white">Estas en: {currentCountry}</h5>
-				<h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 sm:mb-8 text-white">{stateToShow.name}</h1>
-				<div className="flex flex-col space-y-4 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl mx-auto px-4">
-					{stateToShow.descriptions.map((description, index) => (
-						<div
-							key={index}
-							className="bg-white bg-opacity-20 backdrop-blur-lg rounded-lg p-3 sm:p-4 text-sm sm:text-base text-white shadow-lg transition-all duration-500 hover:scale-105">
-							{description}
-						</div>
-					))}
+			return (
+				<div className={`w-full min-h-screen flex flex-col items-center justify-center p-4 sm:p-6 md:p-8 transition-colors duration-1000 ${stateToShow.background}`}> 		
+					{countdownTime < 10 ? <h5 className="text-white">A punto de cambiar de estado...</h5> : ""}
+					{secondsRem < 10 ? <h5 className="text-white">Tiempo restante para el siguiente estado: {minutesRem}:0{secondsRem} </h5> : <h5 className="text-white">Tiempo restante para el siguiente estado: {minutesRem}:{secondsRem} </h5>}
+					
+					<h1 className="text-1xl font-bold mb-6 sm:mb-8 text-white">Estas en: {country}</h1>
+					<h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 sm:mb-8 text-white">{stateToShow.name}</h1>
+					<div className="flex flex-col space-y-4 w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl mx-auto px-4">
+						{stateToShow.descriptions.map((description, index) => (
+							<div
+								key={index}
+								className="bg-white bg-opacity-20 backdrop-blur-lg rounded-lg p-3 sm:p-4 text-sm sm:text-base text-white shadow-lg transition-all duration-500 hover:scale-105">
+								{description}
+							</div>
+						))}
+					</div>
+					{/* <div className="mt-5">
+						<Calendar />
+					</div> */}
 				</div>
-				<div className="mt-5">
-					<Calendar />
-				</div>
-			</div>
-		)
+			)
+		} catch (error) {
+			setOnError((error as Error).message)
+		}
+		
 	}
 }
 
--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,17 +1,36 @@
 import { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
+import { getSunrise } from "sunrise-sunset-js"
 
+function getState(sunriseTime: Date, timePicked: Date) {
+	let state = 0;
+    console.log(sunriseTime)
+    console.log(timePicked)
+	while (sunriseTime <= timePicked) {
+		//console.log(sunriseTime)
+		state += 1;
+		if (state == 6) {
+			state = 1;
+		}
+		sunriseTime.setMinutes(sunriseTime.getMinutes() + 24);
+	}
+	return state; 
+}
 
 const Calendar = () => {
     const [calendarDate, setCalendarDate] = useState(new Date()); // default calendar date
-    const [datePicked, setDatePicked] = useState(new Date().toISOString().split("T")[0]);
+    const [datePicked, setDatePicked] = useState(new Date());
     const [sunriseTime, setSunriseTime] = useState("")
+    const [state, setState] = useState<number | null>(null);
+    const [hourPicked, setHourPicked] = useState<string | null>(null);
+    
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
+            const sunriseTime = getSunrise(position.coords.latitude, position.coords.longitude)
 
-            const apiUrl = `https://api.sunrisesunset.io/json?lat=${lat}&lng=${lon}&date=${datePicked}`
+            const apiUrl = `https://api.sunrisesunset.io/json?lat=${lat}&lng=${lon}&date=${datePicked.toISOString().split("T")[0]}`
 			fetch(apiUrl)
             .then(response => {
                 if (response.ok) {
@@ -22,7 +41,11 @@ const Calendar = () => {
             })
             .then(data => {
                 setSunriseTime(data.results.sunrise)
-                console.log(calendarDate.toISOString())
+                const str_sunrise = data.results.sunrise.split(" ")[0];
+                const str_sunriseDay = datePicked.toISOString().split("T")[0];
+                const newDate = new Date(str_sunriseDay + "T0" + str_sunrise);
+                setState(getState(newDate, datePicked));
+                //console.log(new Date.parse(datePicked.toISOString().split("T")[0] + " " + str_sunrise.split(" ")[0]))
             })
         })
 
@@ -31,13 +54,15 @@ const Calendar = () => {
 
     function changeDate(date:Date) {
         setCalendarDate(date)
-        setDatePicked(date.toISOString().split("T")[0])
+        setDatePicked(date);
+        setHourPicked(date.toISOString().split("T")[0])
     }
 
     return (
         <>
-			<DatePicker className='text-black' showIcon selected={calendarDate} onChange={(date: Date | null) => {if (date) changeDate(date)}}></DatePicker>
-            <p className='text-black mt-5'><b>Hora de amanecer el {datePicked}:</b> {sunriseTime}</p>
+			<DatePicker showTimeSelect showIcon selected={calendarDate} onChange={(date: Date | null) => {if (date) changeDate(date)}}></DatePicker>
+            <p className='mt-5'><b>Hora de amanecer el {datePicked.toISOString().split("T")[0]}:</b> {sunriseTime}</p>
+            <p><b>Estado a las {hourPicked}: </b>{state}</p>
 		</>
     )
 }
