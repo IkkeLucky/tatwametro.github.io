acaf839eb462d7190bc820f3d9e564f01e29d5d4
gon2410 <goonolivera@hotmail.com>
2024-12-28 12:35:10 -0300

    added style

--- src/App.tsx
+++ src/App.tsx
@@ -31,13 +31,24 @@ function App() {
 
 	if (choose == "none") {
 		return (
-			<div className="w-full min-h-screen flex flex-col items-center justify-center">
-				<CountryDropdown value={country} onChange={(val) => {setCountry(val); setChoose("manual")}} />
-				<button className="btn border-green-500 p-1 mt-10" onClick={getPosition}>Por ubicacion</button>
-				<div className="mt-5">
-					<Calendar />
-				</div>
-			</div>
+			<>
+				<header>
+				<h1 className="mt-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Tatwámetro</h1>
+				</header>
+			
+				<main>
+					<div className="w-full min-h-screen flex flex-col items-center justify-center">
+						<label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seleccione un país</label>
+						<CountryDropdown className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={country} onChange={(val) => {setCountry(val); setChoose("manual")}} />
+						<hr />
+						<button className="mt-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={getPosition}>Ubicación precisa</button>
+						<hr />
+						{/* <div className="mt-10">
+							<Calendar />
+						</div> */}
+					</div>
+				</main>
+			</>
 		)
 	} else if (choose == "auto") {
 		return (
